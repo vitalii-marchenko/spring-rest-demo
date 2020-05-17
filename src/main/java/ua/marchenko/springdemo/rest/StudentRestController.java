d3a389a154453400638e3903c81dc5cdc75c1d83
Vitaly Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-05-17 14:06:43 +0300

    Remove unsused imports

--- src/main/java/ua/marchenko/springdemo/rest/StudentRestController.java
+++ src/main/java/ua/marchenko/springdemo/rest/StudentRestController.java
@@ -1,8 +1,9 @@
 package ua.marchenko.springdemo.rest;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ua.marchenko.springdemo.entity.Student;
 
 import javax.annotation.PostConstruct;
